f708358e0f5d1f983f7126a2cf6ad439e83efe9c
Gabriel Vicente <gabrielvicente.m@gmail.com>
2019-03-26 01:21:46 -0300

    Add files via upload

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (!_context.TodoItems.Any())
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item 1", Details = "Details 1" });
+                _context.TodoItems.Add(new TodoItem { Name = "Item 2", Details = "Details 2" });
+                _context.TodoItems.Add(new TodoItem { Name = "Item 3", Details = "Details 3" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItemsAsync()
+        {
+            return await _context.TodoItems.OrderBy(o => o.Id).ToListAsync();
+        }
+
+        // GET: api/Todo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItemAsync(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+                return NotFound();
+
+            return todoItem;
+        }
+
+        // POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItemAsync(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItemAsync), new { id = item.Id }, item);
+        }
+
+        // PUT: api/Todo/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItemAsync(long id, TodoItem item)
+        {
+            if (id != item.Id)
+                return BadRequest();
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Todo/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItemAsync(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+                return NotFound();
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Details { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
